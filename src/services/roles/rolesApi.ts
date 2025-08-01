15ccf3c57f0e87f4454017cba66bcff689592eaa
Abdelquodr <44165332+abdelquodr@users.noreply.github.com>
2025-08-01 22:10:48 +0100

    update base url

--- src/services/roles/rolesApi.ts
+++ src/services/roles/rolesApi.ts
@@ -2,10 +2,14 @@
 import { apiFetch } from "./api";
 import { Role } from "./types";
 
+// const BASE_URL =
+//   process.env.NODE_ENV === "production"
+//     ? "https://medusa-backend-8vhp9gzpf-abdelquodrs-projects.vercel.app/"
+//     : "http://localhost:3001"; // supposed to be in the environment .env
+
 const BASE_URL =
-  process.env.NODE_ENV === "production"
-    ? "https://medusa-backend-8vhp9gzpf-abdelquodrs-projects.vercel.app/"
-    : "http://localhost:3001"; // supposed to be in the environment .env
+  "https://medusa-backend-8vhp9gzpf-abdelquodrs-projects.vercel.app";
+
 export const getRoles = async (): Promise<Role[]> => {
   const url = `${BASE_URL}/api/roles`;
   return apiFetch<Role[]>(url);
